9be85b8590ea4231297a5ece7dc909624d424ae0
Jimmy Chen <jmchen999@gmail.com>
2026-07-29 19:58:54 +0800

    modify

--- frontend/src/components/Sidebar.jsx
+++ frontend/src/components/Sidebar.jsx
@@ -40,7 +40,7 @@ const Sidebar = () => {
           <img
             src={techluxLogo}
             alt="TECHLUX Logo"
-            className="h-auto w-[230px] max-w-none"
+            className="h-auto w-[360px] max-w-none"
           />
           <p className="text-center text-base font-medium text-slate-200">
             社區充電能源管理系統
